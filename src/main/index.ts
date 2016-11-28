249cecf6be93d2c1bbd2805ea286428136015648
Dennis Schwartz <dennis@repositive.io>
2016-11-28 17:16:06 +0000

    First version works.

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,2 +1,35 @@
+export function toBoolString(query): string {
+  /*
+   Stringify query object into Boolean Algebra string:
+   1. Recursively iterate over query object (tree structure) (TODO: Validate object. -> Detect circular structures?)
+   2. Keys starting with $ are root nodes.
+   3. Concatenate leafs using the root's operator.
+   */
 
-console.log('Hello World');
+  const key = Object.keys(query)[0]; // Only has one key
+  if (query.hasOwnProperty(key)) {
+    /*
+     Case 1: Child is node with children
+     */
+    if (key.startsWith('$')) {
+      const operator = key.slice(1).toUpperCase();
+      const children = query[key];
+      let result = [];
+      for (let i = 0; i < children.length; i++) {
+        result.push(toBoolString(children[i]));
+      }
+      const temp = result.join(` ${operator} `);
+      if (operator === 'NOT') return `${operator} ${temp}`;
+      return "(" + temp + ")";
+    }
+    /*
+     Case 2: Child is leaf node
+     */
+    // Deal with white space and add predicate for non-text searches.
+    const value = /\s+/.test(query[key]) ? `"${query[key]}"` : query[key];
+    if (key === 'text') {
+      return value;
+    }
+    return `${key}:${value}`;
+  }
+}
