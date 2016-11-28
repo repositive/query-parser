ace88fc807ea47ef021b83fb49529404b56775d6
Dennis Schwartz <dennis@repositive.io>
2016-11-28 13:18:09 +0000

    Rename and add mock test.

--- src/test/tests.ts
+++ src/test/tests.ts
@@ -2,5 +2,23 @@
  * Created by dennis on 28/11/2016.
  */
 
-import * as t from 'tape';
+import * as test from 'tape';
+import * as queryalizer from '../main/index';
 
+const input1 = {
+  $and: [
+    {text: 'cancer'},
+    {assay: 'X'},
+    {$or: [
+      {collection: 'Y'},
+      {collection: 'Z'}
+    ]}
+  ]
+};
+
+test('serialize', function (t) {
+  t.plan(1);
+
+  const result = queryalizer(input1);
+  t.equal(typeof result, 'string');
+});
