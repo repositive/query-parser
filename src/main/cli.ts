7ddbdb48c3dc2d678c3aaaf034a3eb6c248d4a36
Dennis Schwartz <dennis@repositive.io>
2016-11-29 09:56:03 +0000

    Removed unnecessary comment.

--- src/main/cli.ts
+++ src/main/cli.ts
@@ -5,14 +5,3 @@ let argv = require('minimist')(process.argv.slice(2));
 import { toBoolString } from './index';
 
 process.stdout.write(toBoolString(JSON.parse(argv.query)));
-
-// console.log(toBoolString({
-//   $and: [
-//     {text: 'cancer'},
-//     {assay: 'X'},
-//     {$or: [
-//       {collection: 'Y'},
-//       {collection: 'Z'}
-//     ]}
-//   ]
-// }));
