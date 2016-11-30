35366e51e6fb894a083ca1fb89d2256bc59707ff
Ruben Paz <me@ruben.io>
2016-11-30 16:29:03 +0000

    Extract quoted strings

---
+++ src/main/parsers/extract-quoted.ts
@@ -0,0 +1,44 @@
+import {head, tail as _tail} from 'ramda';
+import {Token} from './base-parser';
+
+const tail:any = _tail;
+
+function extractQuotedWord(input: string, acc: string = ''): string {
+  const h = head(input);
+  if (h === '') {
+    throw new Error(`No closing quote in ${acc}`)
+  }
+  else if(h === '"') {
+    return acc;
+  }
+  else {
+    return extractQuotedWord(tail(input), acc + h);
+  }
+}
+
+function extractQuotedWords(input: string, acc: string[] = []) {
+  const h = head(input);
+  if (h === '') {
+    return acc;
+  }
+  else if (h === '"') {
+    const word = extractQuotedWord(tail(input));
+    acc.push(`"${word}"`);
+    return extractQuotedWords(input.substring(word.length + 2), acc);
+  }
+  else {
+    return extractQuotedWords(tail(input), acc);
+  }
+}
+
+export default function extractQuoted(input: string, acc: Token[] = []) {
+  const words = extractQuotedWords(input);
+
+  return words.map((w):Token => {
+    return {
+      type: 'term',
+      position: input.indexOf(w),
+      term: w.replace(/\"/g,'')
+    }
+  })
+}
---
+++ src/test/parsers/query-parser.spec.ts
@@ -0,0 +1,59 @@
+
+import * as test from 'tape';
+import {parseString as parse} from '../../main/parsers/query-parser';
+import extractQuoted from '../../main/parsers/extract-quoted';
+
+test('should parse a simple query',(t) => {
+  t.plan(1);
+  const result = parse('cancer');
+  t.deepEqual(result, {value: { text: 'cancer'}});
+});
+
+test('extract quoted of simple string', t => {
+  t.plan(1);
+  const result = extractQuoted('"test"');
+  t.deepEqual(result, [{type: 'term', position: 0, term: 'test'}]);
+})
+
+test('extract multiple quoted entries from string', t => {
+  t.plan(1);
+  const result = extractQuoted('"1" n "2"');
+
+  t.deepEqual(result, [
+    {type: 'term', position: 0, term: '1'},
+    {type: 'term', position: 6, term: '2'}
+  ])
+})
+
+test.skip('should parse a simple query with multiple values', (t) => {
+  t.plan(2);
+  const result1 = parse('simple query');
+  t.deepEqual(result1, {$and: [{text: 'simple'}, {text: 'query'}]});
+
+  const result2 = parse('another simple query');
+  t.deepEqual(result2, {$and: [{text: 'another'}, {text: 'simple'}, {text: 'query'}]});
+});
+
+
+test.skip('should parse a match exactly simple query', (t) => {
+  t.plan(1);
+  const result = parse('"simple query"')
+  t.deepEqual(result, {$and: [{text: 'simple query'}]});
+});
+
+test.skip('should parse a match exactly multiple times', (t) => {
+  t.plan(1);
+  const result = parse('"first match" "second match"');
+  t.deepEqual(result, {$and: [{text: 'first match'}, {text: 'second match'}]});
+});
+
+test.skip('should parse a exact match with loose interpolated', (t) => {
+  t.plan(1);
+  const result = parse('"first match" second match "third match"');
+  t.deepEqual(result, {$and: [
+    {text: 'first match'},
+    {text: 'third match'},
+    {text: 'second'},
+    {text: 'match'}
+  ]});
+});
