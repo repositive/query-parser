d53a77f436f0944f6905f3ff56ae1423d72715ea
Ruben Paz <me@ruben.io>
2016-11-30 15:49:32 +0000

    Basic parser definition

---
+++ src/main/parsers/base-parser.ts
@@ -0,0 +1,15 @@
+export interface Token {
+  type: 'bo' | 'term' | 'filter';
+  position: number;
+  term: string;
+}
+
+export function isToken(o: any): o is Token {
+  return typeof o === 'object' &&
+    [ 'bo'
+    , 'term'
+    , 'filter'
+    ].indexOf(o.type) !== -1
+}
+
+export type Parser = (input: string, acc: Token[]) => Token[];
