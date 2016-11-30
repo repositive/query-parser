80f524b36cb353b37d160b5b858c3ecc2dc046bc
Dennis Schwartz <dennis@repositive.io>
2016-11-30 18:08:32 +0000

    Moved elastic serialiser to tree structure and new file.

--- src/main/serializers/string.ts
+++ src/main/serializers/string.ts
@@ -9,12 +9,16 @@ export function toBoolString(tree:BTree<SearchNode>): string {
     const value = tree.value;
 
     // 1. Value is filter or text
-    if (isFilter(value)) return `${value.predicate}:${value.text}`;
-    if (isTerm(value)) return value.text;
+    if (isFilter(value)) return `${value.predicate}:${quotes(value.text)}`;
+    if (isTerm(value)) return quotes(value.text);
 
     // 2. Value is operator
     if (isBooleanOperator(value)) {
       if (value.operator === 'NOT') return `${value.operator} ${toBoolString(tree.right)}`;
       return `(${toBoolString(tree.left)} ${value.operator} ${toBoolString(tree.right)})`
     }
+}
+
+function quotes(text) {
+  return /\s+/.test(text) ? `"${text}"` : text;
 }
