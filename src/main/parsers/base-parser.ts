9642af3e1d8838d65449ba732b9593199c27b917
Dennis Schwartz <dennis@repositive.io>
2016-12-01 14:09:39 +0000

    Added predicate type to base-parser.

--- src/main/parsers/base-parser.ts
+++ src/main/parsers/base-parser.ts
@@ -1,15 +1,17 @@
 export interface Token {
-  type: 'bo' | 'term' | 'filter' | 'group';
+  type: 'bo' | 'term' | 'filter' | 'group' | 'predicate';
   from: number;
   to: number;
   term: string;
+  predicate?: string;
 }
 
 export function isToken(o: any): o is Token {
   return typeof o === 'object' &&
-    [ 'bo'
-    , 'term'
-    , 'filter'
+    [ 'bo',
+      'term',
+      'filter',
+      'predicate'
     ].indexOf(o.type) !== -1
 }
 
