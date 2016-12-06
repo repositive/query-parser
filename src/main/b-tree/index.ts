0bcaa42c19dcbdf199ce2d36f8f75d2cd179a5c0
Dennis Schwartz <dennis@repositive.io>
2016-12-06 16:32:41 +0000

    Added addFilter and removeFilter functions.

--- src/main/b-tree/index.ts
+++ src/main/b-tree/index.ts
@@ -1,4 +1,5 @@
 import {v4 as uuid} from 'uuid';
+import {append} from 'ramda';
 
 export interface BTree<O, T> {
   value: O;
@@ -7,15 +8,16 @@ export interface BTree<O, T> {
 }
 
 export function isBTree<O, T>(o: any): o is BTree<O, T> {
-  return typeof o === 'object' && 
+  return o &&
+    typeof o === 'object' &&
     o.value !== undefined &&
     o.value !== null;
 }
 
-export function fold<O, T, R>(tree: BTree<O, T> | T, f: (R, tree: O | T) => R, acc: R): R {
+export function fold<O, T, R>(tree: BTree<O, T> | T, f: (R, tree: BTree<O, T> | T) => R, acc: R): R {
   if (!tree) return acc;
   if (isBTree(tree)) {
-    const newAcc = f(acc, tree.value);
+    const newAcc = f(acc, tree);
     return fold(tree.right, f, fold(tree.left, f, newAcc));
   }
   else {
@@ -46,21 +48,27 @@ export function mapLeafs<O,T>(tree: BTree<O,T> | T, f: (leaf: T) => T): BTree<O,
   }
 }
 
+// export function mapPath<O,T>(tree: BTree<O,T> | T, f: (tree: BTree<O,T>) => BTree<O,T>, path: string[]): BTree<O,T> | T {
+//   if (path.length === 0) return tree;
+// }
+
+// export function getPath<O,T>(tree: )
+
 // export function map<O,T>(f: (tree: BTree<O, T> | T) => BTree<O, T> | T, tree: BTree<O,T> | T): BTree<O, T> | T {
 //   return {
 //
 //   }
 // }
 
-export function filter<O, T>(tree: BTree<O, T> | T, f: (val: O | T) => boolean): BTree<O, T> {
+export function filter<O, T>(tree: BTree<O, T> | T, f: (val: BTree<O,T> | T) => boolean): (BTree<O, T> | T)[] {
   return fold(tree, (acc, val) => {
     if (f(val)) {
-      return val
+      return append(val, acc);
     }
     else {
       return acc;
     }
-  }, null);
+  }, []);
 }
 
 //export function map<T, R>(f: (T) => R, tree: T): BTree<R, R> {
@@ -96,7 +104,7 @@ export default class BTreeImp<O, T> implements BTree<O, T> {
     return fold(this, f, acc);
   }
 
-  filter(f: (val: O | T) => boolean) {
+  filter(f: (val: BTree<O,T> | T) => boolean) {
     return filter(this, f);
   }
 
--- src/tests/operations/operations.spec.ts
+++ src/tests/operations/operations.spec.ts
@@ -2,7 +2,7 @@ import * as test from 'tape';
 import {BTree} from '../../main/b-tree/index';
 import {BooleanOperator, BTreeLeaf, BBTree} from '../../main/b-exp-tree';
 import {insertFilter} from '../../main/operations/insert-filter'
-import {getPath} from '../../main/operations/filters';
+import {getPath, addFilter, removeByID, removeFilter} from '../../main/operations/filters';
 
 
 const simpleTree1: BTreeLeaf = {
@@ -66,35 +66,35 @@ const tree2: BTree<BooleanOperator, BTreeLeaf> = {
 
 
 const tree3: BTree<BooleanOperator, BTreeLeaf> =
-{
-  value: 'AND',
-  left: {
-    predicate: 'access',
-    text: 'open'
-  },
-  right: {
+  {
     value: 'AND',
     left: {
-      text: 'glaucoma'
+      predicate: 'access',
+      text: 'open'
     },
-    right: <BTree<BooleanOperator, BTreeLeaf>> {
+    right: {
       value: 'AND',
       left: {
-        value: 'NOT',
-        right: {
-          predicate: 'assay',
-          text: 'RNA-Seq'
-        }
+        text: 'glaucoma'
       },
-      right: {
-        predicate: 'collection',
-        text: 'X'
+      right: <BTree<BooleanOperator, BTreeLeaf>> {
+        value: 'AND',
+        left: {
+          value: 'NOT',
+          right: {
+            predicate: 'assay',
+            text: 'RNA-Seq'
+          }
+        },
+        right: {
+          predicate: 'collection',
+          text: 'X'
+        }
       }
     }
-  }
-};
+  };
 
-const treeWithIDs: BBTree = {
+const wIDsRemoved: BBTree = {
   _id: '1',
   value: 'AND',
   left: {
@@ -111,6 +111,42 @@ const treeWithIDs: BBTree = {
         _id: '5',
         predicate: 'assay',
         text: 'X'
+      }
+    },
+    right: {
+      _id: '4',
+      predicate: 'collection',
+      text: 'X'
+    }
+  }
+};
+
+const treeWithIDs: BBTree = {
+  _id: '1',
+  value: 'AND',
+  left: {
+    _id: '7',
+    text: 'glaucoma'
+  },
+  right: {
+    _id: '2',
+    value: 'AND',
+    left: {
+      _id: '3',
+      value: 'NOT',
+      left: null,
+      right: {
+        _id: '8',
+        value: 'AND',
+        left: {
+          _id: '9',
+          text: 'new!'
+        },
+        right: {
+          _id: '5',
+          predicate: 'assay',
+          text: 'X'
+        }
       }
     },
     right: {
@@ -168,6 +204,25 @@ test.skip('Add filters to complex tree', t => {
 test('Should return path to id', t => {
   t.plan(3);
   t.deepEquals(['1','2','3'], getPath(treeWithIDs, '3'));
-  t.deepEquals(['1','2','3','5'], getPath(treeWithIDs, '5'));
+  t.deepEquals(['1','2','3','8','5'], getPath(treeWithIDs, '5'));
   t.deepEquals(['1','5','7'], getPath(twoOrs, '7'));
+});
+
+test.skip('Add predicates', t => {
+  t.plan(1);
+  const res = addFilter(treeWithIDs, 'assay', 'Y');
+  t.deepEquals(treeWithIDs, res);
+});
+
+test('Remove nodes', t => {
+  t.plan(1);
+  const res = removeByID(treeWithIDs, '7');
+  t.deepEquals(treeWithIDs, res);
+});
+
+test('remove filters', t => {
+  t.plan(1);
+  const res = removeFilter(treeWithIDs, 'collection', 'X');
+  console.log(JSON.stringify(res,null,2));
+  t.deepEquals(treeWithIDs, res);
 });
